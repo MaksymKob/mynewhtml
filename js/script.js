b8255d00d8625f1ba73b873cc37bc3292fa0a722
Maxym <example@gmail.com>
2023-08-26 23:20:00 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// snake_case
+// UPPER_SNAKE_CASE
+// Kebab-case
+// PascalCase
+
+const answers = [];
+
+answers[0] = prompt('What is your name?', '');
+answers[1] = prompt('What is your last name?', '');
+answers[2] = prompt('How old are you?', '');
